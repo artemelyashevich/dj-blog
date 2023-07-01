42d98ea225d39e97595d1caf8cef325bae88a3a5
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2023-07-01 12:37:39 +0300

    refactoring

---
+++ app/blog/utils.py
@@ -0,0 +1,27 @@
+menu = [
+    {'title': 'Главная', 'path': '/'},
+    {'title': 'Добавить статью', 'path': '/add-post'},
+    {'title': 'Войти', 'path': '/login'},
+]
+
+category_list = [
+    {'title': 'Люди', 'path': 'man'},
+    {'title': 'Электроника', 'path': 'el'},
+    {'title': 'Книги', 'path': 'book'}
+]
+
+
+class DataMixin:
+
+    def get_user_context(self, **kwargs):
+        context = kwargs
+
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(1)
+        else:
+            user_menu.pop(2)
+
+        context['navs'] = user_menu
+        context['cats'] = category_list
+        return context
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,56 +1,25 @@
-from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView
 from .forms import AddPostForm
 from .models import Post
-
-menu = [
-    {'title': 'Главная', 'path': '/'},
-    {'title': 'Добавить статью', 'path': '/add-post'},
-    {'title': 'Войти', 'path': '/login'},
-]
-
-category_list = [
-    {'title': 'Люди', 'path': 'man'},
-    {'title': 'Электроника', 'path': 'el'},
-    {'title': 'Книги', 'path': 'book'}
-]
+from .utils import DataMixin
 
 
-class PostHome(ListView):
+class PostHome(DataMixin, ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['navs'] = menu
-        context['title'] = 'Главная'
-        context['cats'] = category_list
-        return context
+        c = self.get_user_context(title='Главная')
+        return dict(list(context.items()) + list(c.items()))
 
     def get_queryset(self):
         return Post.objects.filter(is_published=True)
 
 
-class PostCategory(ListView):
-    model = Post
-    template_name = 'blog/index.html'
-    context_object_name = 'posts'
-    allow_empty = False
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = str('Категория ' + str(context['posts'][0].category))
-        context['navs'] = menu
-        context['cat_selected'] = self.kwargs['cat']
-        context['cats'] = category_list
-        return context
-
-    def get_queryset(self):
-        return Post.objects.filter(category=self.kwargs['cat'].upper(), is_published=True)
-
-
-class PostPage(DetailView):
+class PostPage(DataMixin, DetailView):
     model = Post
     template_name = 'blog/post.html'
     pk_url_kwarg = 'post_id'
@@ -58,20 +27,33 @@ class PostPage(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Пост'
-        context['navs'] = menu
-        context['cats'] = category_list
-        return context
+        c = self.get_user_context(title='Статья')
+        return dict(list(context.items()) + list(c.items()))
 
 
-class PostAdd(CreateView):
-    form_class = AddPostForm
-    template_name = 'blog/add-post.html'
-    success_url = reverse_lazy('/')
+class PostCategory(DataMixin, ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        context['navs'] = menu
-        context['cats'] = category_list
-        return context
+        c = self.get_user_context(title='Категория ' + str(context['posts'][0].category),
+                                  cat_selected=self.kwargs['cat'])
+        return dict(list(context.items()) + list(c.items()))
+
+    def get_queryset(self):
+        return Post.objects.filter(category=self.kwargs['cat'].upper(), is_published=True)
+
+
+class PostAdd(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/add-post.html'
+    login_url = '/admin/'
+    raise_exception = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c.items()))
